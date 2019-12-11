7152b5db7e958ce37978946710f981cd737a9bd9
Haitham <haitham.fallatah.1990@gmail.com>
2019-12-11 17:05:40 +0100

    Add space before main function

--- bikeshare.py
+++ bikeshare.py
@@ -153,6 +153,7 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
+
 def main():
     while True:
         city, month, day = get_filters()
